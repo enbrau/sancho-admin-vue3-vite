a4a75469f5514cda1381f2eb436ec803c322963c
enbrau <admin@enbrau.com>
2022-06-01 17:32:14 +0800

    feat layout structrure

--- src/layout/index.vue
+++ src/layout/index.vue
@@ -1,13 +1,80 @@
 <template>
-  <router-view v-slot="{ Component }">
-    <keep-alive>
-      <component :is="Component" :key="undefined"  />
-    </keep-alive>
-  </router-view>
+  <div class="app-wrapper" :class="appStatus">
+    <div class="sidebar-container">
+
+    </div>
+    <div class="view-container">
+      <div class="view-head">
+        <div class="navbar-container">
+          
+        </div>
+        <div class="tagbar-container">
+
+        </div>
+      </div>
+      <div class="view-body">
+        <router-view v-slot="{ Component }">
+          <keep-alive>
+            <component :is="Component" :key="undefined"  />
+          </keep-alive>
+        </router-view>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-
+  computed: {
+    appStatus() {
+      return {}
+    }
+  }
 }
 </script>
+
+<style lang="scss">
+.app-wrapper {
+  position: relative;
+  height: 100%;
+  width: 100%;
+
+  .sidebar-container {
+    transition: width 0.28s;
+    width: var(--sancho-sidebar-width);
+    position: fixed;
+    font-size: 0px;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    z-index: 1001;
+    overflow: hidden;
+  }
+
+  .view-container {
+    min-height: 100%;
+    transition: margin-left 0.28s;
+    margin-left: var(--sancho-sidebar-width);
+    position: relative;
+
+    .view-head {
+      .navbar-container {
+        height: var(--sancho-navbar-height);
+      }
+      .tagbar-container {
+        height: var(--sancho-tagbar-height);
+      }
+    }
+  }
+
+  &.sidebar-collapse {
+    .sidebar-container {
+      width: var(--sancho-sidebar-width-collapse);
+    }
+
+    .view-container {
+      margin-left: var(--sancho-sidebar-width-collapse);
+    }
+  }
+}
+</style>
