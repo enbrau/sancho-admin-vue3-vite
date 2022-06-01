f45e6288bfef142ab1f3c3f24b0c26395832132d
enbrau <admin@enbrau.com>
2022-06-01 13:47:14 +0800

    feat import element-plus

--- src/main.js
+++ src/main.js
@@ -10,6 +10,30 @@ const app = createApp(App)
   .use(store)
   .use(i18n)
 
+// UI Library: ElementPlus
+import ElementPlus from 'element-plus'
+import 'element-plus/theme-chalk/index.css'
+import en from 'element-plus/lib/locale/lang/en'
+import zhCn from 'element-plus/lib/locale/lang/zh-cn'
+import zhTw from 'element-plus/lib/locale/lang/zh-tw'
+import * as ElIconModules from '@element-plus/icons'
+for(let iconName in ElIconModules){
+  app.component(iconName, ElIconModules[iconName])
+}
+app.use(ElementPlus, {
+  size: store.state.app.size,
+  locale: (function(locale){
+    switch(locale) {
+      case 'zh-cn':
+        return zhCn
+      case 'zh-tw':
+        return zhTw
+      default:
+        return en
+    }
+  }(store.state.app.locale))
+})
+
 import { preloadHook } from './hooks'
 preloadHook.promise(app).then(() => {
   app.mount('#app')
